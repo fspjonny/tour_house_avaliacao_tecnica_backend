42c0e582362f84d969923112c5622876f0ce023d
fspjonny <fabiosilvapedro@yahoo.com.br>
2024-11-16 19:39:20 -0300

    :sparkles: feat: filtros avançados aplicados no endpoint foram aplicados no admin de Funcionários.

--- funcionarios/admin.py
+++ funcionarios/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Funcionario
 
-admin.site.register(Funcionario)
+@admin.register(Funcionario)
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = ['nome_completo', 'departamento', 'cidade', 'ativo']
+    list_filter = ['departamento', 'cidade', 'ativo']
+    search_fields = ['nome_completo']
