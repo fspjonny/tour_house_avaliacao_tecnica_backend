b497550375e7b43b5aab5aff068dbf1dfc8c2606
fspjonny <fabiosilvapedro@yahoo.com.br>
2024-11-20 17:06:14 -0300

    :recycle: refactor: Melhoria na segurança dos endpoints.

--- core/settings.py
+++ core/settings.py
@@ -39,7 +39,6 @@ REST_FRAMEWORK = {
     ),    
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
